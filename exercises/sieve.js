0377d5539f9ec0ed6b259e414364151dc53abd8b
Cristian Leu <cristianleu@WS-CJ-U2-1107.local>
2017-12-08 17:50:54 +0200

    solved sieve

--- exercises/sieve.js
+++ exercises/sieve.js
@@ -14,19 +14,15 @@ const makeArray = function() {
 const removeMultiples = function( array, currentElement ) {
   var n = 2;
 
-  while( currentElement*n < 100 ) {
+  while( currentElement*n <= 100 ) {
     var removalIndex = array.indexOf( currentElement * n );
-    if( removalIndex ) {
+    if( removalIndex !== -1 ) {
       array.splice( removalIndex, 1 );
-        console.log( "multiple of ", currentElement, ":", currentElement * n , " removed " )
     }
 
     n = n + 1;
-    console.log( "arr",array )
   }
 
-  console.log( "removed " + currentElement + "'s multiples " )
-  //console.log( "remaining", array );
   return array;
 }
 
@@ -34,16 +30,13 @@ const findPrimes = function( array ) {
   var currentElement = 2; // p from wikipedia
   var currentIndex = 0;
 
-  // while ( currentElement * 2 < 100  ) {
-  //   array = removeMultiples( array, currentElement );
-  //   currentIndex = currentIndex + 1;
-  //   currentElement = array[ currentIndex ];
-  // }
+  while ( currentElement * 2 <= 100  ) {
+    array = removeMultiples( array, currentElement );
+    currentIndex = currentIndex + 1;
+    currentElement = array[ currentIndex ];
+  }
 
-  wo2 = removeMultiples( array, 2 );
-  wo3 = removeMultiples( wo2, 3 );
-
-  return wo3;
+  return array;
 }
 
 console.log( findPrimes( makeArray() ) )
